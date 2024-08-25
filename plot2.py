5d4eb5240c69486f9dd3df2207f1fcea147846d8
Paulo Júnior <70821554+juniorrkcm@users.noreply.github.com>
2024-08-24 21:37:11 -0400

    Add files via upload

---
+++ plot2.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Leia os dados do arquivo CSV
+data = pd.read_csv('fibonacci_times_matrix.csv')
+
+# Converta a coluna 'Terms' para um tipo de dado inteiro
+data['Terms'] = data['Terms'].astype(int)
+
+# Crie o gráfico de linha
+plt.figure(figsize=(10, 6))
+plt.plot(data['Terms'], data['ExecutionTime'], marker='o', linestyle='-', color='b')
+plt.xlabel('Number of Terms')
+plt.ylabel('Execution Time (seconds)')
+plt.title('Execution Time of Fibonacci Calculation vs Number of Terms')
+plt.grid(True)
+
+# Exibe o gráfico
+plt.show()
